995d9da5eda94a622e3aaa58cc3344da1b574f2f
Endkind <endkind.ender@gmail.com>
2023-11-18 17:56:40 +0100

    latest

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ if [ ! -f "/endkind/server.jar" ]; then
 fi
 
 while true; do
-    java -Xms$MIN_RAM -Xmx$MAX_RAM $JAVA_FLAGS -jar /endkind/server.jar $VELOCITY_FLAGS
+    java -Xms$MIN_RAM -Xmx$MAX_RAM $JAVA_FLAGS -Dcom.mojang.eula.agree=$MINECRAFT_EULA -jar /endkind/server.jar $FOLIA_FLAGS --nogui
     if [ $? -ne 0 ]; then
         exit 1
     fi
